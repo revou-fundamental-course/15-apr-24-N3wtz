e2673ebcc0575a89dacb8ae6e12819adc30696fe
Twenz <118372630+N3wtz@users.noreply.github.com>
2024-04-23 19:17:38 +0800

    Second Commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 function convertTemperature() {
-  var temperature = parseFloat(
-    document.getElementById("temperatureInput").value
+  var initialTemperature = parseFloat(
+    document.getElementById("initialTemperatureInput").value
   );
-  var unit = document.getElementById("unitSelect").value;
+  var initialUnit = document.getElementById("initialUnitSelect").value;
+  var finalUnit = document.getElementById("finalUnitSelect").value;
   var result = document.getElementById("result");
   var explanation = document.getElementById("explanation");
 
-  if (isNaN(temperature)) {
+  if (isNaN(initialTemperature)) {
     result.textContent =
-      "Anda belum memasukkan temperatur suhu. Silahkan masukkan terlebih dahulu.";
+      "Anda belum memasukkan suhu awal. Silakan masukkan terlebih dahulu.";
     explanation.textContent = "";
     return;
   }
@@ -16,40 +17,127 @@ function convertTemperature() {
   var convertedTemperature;
   var explanationText;
 
-  if (unit === "celsius") {
-    convertedTemperature = (temperature * 9) / 5 + 32;
-    explanationText =
-      "Cara Perhitungan: (" +
-      temperature +
-      " * 9/5) + 32 = " +
-      convertedTemperature.toFixed(2) +
-      " Fahrenheit.";
-  } else {
-    convertedTemperature = ((temperature - 32) * 5) / 9;
-    explanationText =
-      "Cara Perhitungan: (" +
-      temperature +
-      " - 32) * 5/9 = " +
-      convertedTemperature.toFixed(2) +
-      " Celsius.";
+  if (initialUnit === finalUnit) {
+    result.textContent =
+      initialTemperature +
+      "° " +
+      finalUnit.charAt(0).toUpperCase() +
+      finalUnit.slice(1);
+    explanation.textContent =
+      "Tidak ada konversi yang diperlukan karena satuan suhu awal dan akhir sama.";
+    return;
   }
 
-  result.textContent = convertedTemperature.toFixed(2);
+  if (initialUnit === "celsius") {
+    if (finalUnit === "fahrenheit") {
+      convertedTemperature = (initialTemperature * 9) / 5 + 32;
+      explanationText =
+        "Cara Perhitungan: (" +
+        initialTemperature +
+        " * 9/5) + 32 = " +
+        convertedTemperature.toFixed(2) +
+        "° Fahrenheit.";
+    } else if (finalUnit === "kelvin") {
+      convertedTemperature = initialTemperature + 273.15;
+      explanationText =
+        "Cara Perhitungan: " +
+        initialTemperature +
+        " + 273.15 = " +
+        convertedTemperature.toFixed(2) +
+        "° Kelvin.";
+    } else if (finalUnit === "reaumur") {
+      convertedTemperature = (initialTemperature * 4) / 5;
+      explanationText =
+        "Cara Perhitungan: " +
+        initialTemperature +
+        " * 4/5 = " +
+        convertedTemperature.toFixed(2) +
+        "° Reaumur.";
+    }
+  } else if (initialUnit === "fahrenheit") {
+    if (finalUnit === "celsius") {
+      convertedTemperature = ((initialTemperature - 32) * 5) / 9;
+      explanationText =
+        "Cara Perhitungan: (" +
+        initialTemperature +
+        " - 32) * 5/9 = " +
+        convertedTemperature.toFixed(2) +
+        "° Celsius.";
+    } else if (finalUnit === "kelvin") {
+      convertedTemperature = ((initialTemperature - 32) * 5) / 9 + 273.15;
+      explanationText =
+        "Cara Perhitungan: (" +
+        initialTemperature +
+        " - 32) * 5/9 + 273.15 = " +
+        convertedTemperature.toFixed(2) +
+        "° Kelvin.";
+    } else if (finalUnit === "reaumur") {
+      convertedTemperature = ((initialTemperature - 32) * 4) / 9;
+      explanationText =
+        "Cara Perhitungan: (" +
+        initialTemperature +
+        " - 32) * 4/9 = " +
+        convertedTemperature.toFixed(2) +
+        "° Reaumur.";
+    }
+  } else if (initialUnit === "kelvin") {
+    if (finalUnit === "celsius") {
+      convertedTemperature = initialTemperature - 273.15;
+      explanationText =
+        "Cara Perhitungan: " +
+        initialTemperature +
+        " - 273.15 = " +
+        convertedTemperature.toFixed(2) +
+        "° Celsius.";
+    } else if (finalUnit === "fahrenheit") {
+      convertedTemperature = ((initialTemperature - 273.15) * 9) / 5 + 32;
+      explanationText =
+        "Cara Perhitungan: (" +
+        initialTemperature +
+        " - 273.15) * 9/5 + 32 = " +
+        convertedTemperature.toFixed(2) +
+        "° Fahrenheit.";
+    } else if (finalUnit === "reaumur") {
+      convertedTemperature = ((initialTemperature - 273.15) * 4) / 5;
+      explanationText =
+        "Cara Perhitungan: (" +
+        initialTemperature +
+        " - 273.15) * 4/5 = " +
+        convertedTemperature.toFixed(2) +
+        "° Reaumur.";
+    }
+  } else if (initialUnit === "reaumur") {
+    if (finalUnit === "celsius") {
+      convertedTemperature = (initialTemperature * 5) / 4;
+      explanationText =
+        "Cara Perhitungan: " +
+        initialTemperature +
+        " * 5/4 = " +
+        convertedTemperature.toFixed(2) +
+        "° Celsius.";
+    } else if (finalUnit === "fahrenheit") {
+      convertedTemperature = (initialTemperature * 9) / 4 + 32;
+      explanationText =
+        "Cara Perhitungan: " +
+        initialTemperature +
+        " * 9/4 + 32 = " +
+        convertedTemperature.toFixed(2) +
+        "° Fahrenheit.";
+    } else if (finalUnit === "kelvin") {
+      convertedTemperature = (initialTemperature * 5) / 4 + 273.15;
+      explanationText =
+        "Cara Perhitungan: " +
+        initialTemperature +
+        " * 5/4 + 273.15 = " +
+        convertedTemperature.toFixed(2) +
+        "° Kelvin.";
+    }
+  }
+
+  result.textContent =
+    convertedTemperature.toFixed(2) +
+    "° " +
+    finalUnit.charAt(0).toUpperCase() +
+    finalUnit.slice(1);
   explanation.textContent = explanationText;
-}
-
-function resetForm() {
-  document.getElementById("temperatureInput").value = "";
-  document.getElementById("unitSelect").value = "celsius";
-  document.getElementById("result").textContent = "";
-  document.getElementById("explanation").textContent = "";
-}
-
-function reverseUnit() {
-  var unitSelect = document.getElementById("unitSelect");
-  if (unitSelect.value === "celsius") {
-    unitSelect.value = "fahrenheit";
-  } else {
-    unitSelect.value = "celsius";
-  }
 }
